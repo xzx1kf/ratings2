c4a2d57fea517aa7ac2e169407e39dbc8cc3ed81
Nick Hale <nicholas.hale@gmail.com>
2017-02-28 12:02:02 +0000

    config updates

---
+++ models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Team(db.Model):
+    __tablename__ = 'teams'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<id: {}> {}'.format(self.id, self.name)
