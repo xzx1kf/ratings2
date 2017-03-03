ab7cd748e6f329341c1bdfbb0ea44e8f43628608
Nick Hale <nicholas.hale@gmail.com>
2017-03-03 07:54:00 +0000

    updated admin

--- admin.py
+++ admin.py
@@ -10,7 +10,15 @@ admin = Admin(app, name='Football Ratings', template_mode='bootstrap3')
 class TeamView(ModelView):
     form_excluded_columns = ['team_stats',]
 
-admin.add_view(ModelView(Fixture, db.session))        
+class FixtureView(ModelView):
+    """
+    The on_model_change allows me to insert some logic. So in the case
+    of the fixture save I can calculate fixture stats etc
+    """
+    def on_model_change(self, form, model, is_created):
+        print("{}".format(model.home_team.name))
+
+admin.add_view(FixtureView(Fixture, db.session))
 admin.add_view(ModelView(League, db.session))    
 admin.add_view(TeamView(Team, db.session))
 admin.add_view(ModelView(Team_Stats, db.session))
